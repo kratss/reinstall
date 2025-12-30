b5d8d5d03dc162cdb3ae522aa722295e7ad35cc4
a <a>
2025-12-30 12:46:09 -0700

    Move package list to separate file

--- install.fish
+++ install.fish
@@ -1,6 +1,7 @@
 #!/usr/bin/env fish
 # vim:foldmethod=marker:foldmarker=###,##
 
+### Read install type, packages, and distro
 if test (count $argv) -eq 0 #or contains -- --help $argv
     echo "No arguments provided"
     echo "Installation types:
@@ -13,177 +14,109 @@ end
 
 argparse apps extras 'type=?' -- $argv
 or return 1
-### Packages
-# Basics are commonly installed utilities that may be missing on minimal installs
-# Separated to reduce clutter
-set headless_basics \
-    encfs \
-    plocate
-
-set headless \
-    $headless_basics \
-    curl \
-    dash \
-    fish \
-    fuzzel \
-    fzf \
-    git \
-    gitui \
-    gocryptfs \
-    htop \
-    NetworkManager-tui \
-    neovim \
-    nnn \
-    trash-cli \
-    tree \
-    w3m \
-    wl-clipboard
-
-# Packages sometimes missing from minimal installs
-# lxpolkit: allows authenitcation pop-ups. GNOME and KDE offer alternative polkits
-set gui_basics bluez flatpak lxpolkit
-
-# Packages needed for the basic intended interface
-# python3-pynacl:   dependency for qutebrowser inegration with keepassxc
-set gui \
-    $gui_basics \
-    copyq \
-    foot \
-    fuzzel \
-    grimshot \
-    helvum \
-    imv \
-    light \
-    mako \
-    mpv \
-    pavucontrol \
-    python3-nacl \
-    qutebrowser \
-    sway \
-    tlp \
-    waybar \
-    wlsunset
-
-# Commonly used applications
-set apps gnome-software keepassxc newsboat thunderbird mullvad-browser
-
-# Items only needed for primary device
-# ansible: required for controlling matrix server 
-# just: required for controlling matrix server
-# python3-passlib: required for controlling matrix server
-set extras ansible python3-passlib just mullvad-vpn mullvad-browser rsync vorta
-
-set lazyvim \
-    luarocks \
-    fd-find \
-    fuse
-
 echo "Installation type is: $_flag_type"
 
-if test "$_flag_type" = headless
-    set -g packages $headless $lazyvim
-else if test "$_flag_type" = gui
-    set -g packages $headless $lazyvim $gui $apps
-else if test "$_flag_type" = all
-    set -g packages $headless $lazyvim $gui $apps $extras
-    set -g flatpaks
-    set -g groups Multimedia
-end
-##
-### Package manager detection
-# Also handles varying package names across distros
-echo "Detected package manager: "
-if type dnf 2>/dev/null
-    set -g mngr dnf
-    set -g group "group install"
-    set -a gui foot-terminfo
-else if type apt 2>/dev/null
-    set -g mngr apt-get
-    set packages (string replace NetworkManager-tui "" $packages)
-    set packages (string replace gitui "git-gui" $packages)
-else if type zypper 2>/dev/null
-
-    set -g mngr zypper
-    set -g group "install -t pattern"
-    set packages (string replace nmtui      NetworkManager-tui $packages)
-    set packages (string replace foot-extra foot-extra-terminfo $packages)
-else
-    echo "Failed to detect package manager."
-    exit
+set distro (grep '^ID=' /etc/os-release | cut -d'=' -f2)
+echo "Detected distro: $distro"
+switch $distro
+    case fedora
+        set -g mngr dnf
+    case debian ubuntu
+        set -g mngr apt-get
+    case opensuse-tumbleweed opensuse-leap
+        set -g mngr zypper
+        set -g group "install -t pattern"
+    case "*"
+        echo "Failed to detect distro"
+        exit
 end
 echo "Installing script dependencies"
-sudo $mngr install git curl
+sudo $mngr install git curl yq
 echo "Installing updates"
 sudo $mngr update
+
+# Read packages from YAML
+set headless (yq '.headless[]' packages.yaml)
+set gui (yq '.gui[]' packages.yaml)
+set primary_device (yq '.primary_device[]' packages.yaml)
+set apps (yq '.apps[]' packages.yaml)
+set flatpaks (yq '.flatpaks[]' packages.yaml)
+
+switch $_flag_type
+    case headless
+        set -g packages $headless
+    case gui
+        set -g packages $headless $gui
+    case all
+        set -g packages $headless $gui $apps $primary_device
+        set -g flatpaks
+        set -g groups Multimedia
+end
+# Handles varying package names across distros and other quirks
+switch $distro
+    case debian ubuntu
+        set packages (string replace NetworkManager-tui "" $packages)
+        set packages (string replace gitui "git-gui" $packages)
+    case opensuse-tumbleweed opensuse-leap
+        set packages (string replace nmtui      NetworkManager-tui $packages)
+        set packages (string replace foot-extra foot-extra-terminfo $packages)
+end
 ##
 ### Repo: Mullvad
 if contains mullvad-vpn $packages || or contains mullvad-browser $packages
     echo ""
     echo "Enabling Mullvad repo"
-    if test "$mngr" = dnf
-        sudo dnf config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo --overwrite
-    else if test "$mngr" = apt-get
-        sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc \
-            https://repository.mullvad.net/deb/mullvad-keyring.asc
-        echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc \
-            arch=$( dpkg --print-architecture )] \
-            https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" \
-            | sudo tee /etc/apt/sources.list.d/mullvad.list
-        sudo apt-get update
-    else if test "$mngr" = zypper
-        sudo zypper addrepo -f https://repository.mullvad.net/rpm/stable/mullvad.repo
-    else
-        echo "ERROR: Unable to enable Mullvad repo"
+    switch $distro
+        case dnf
+            sudo dnf config-manager addrepo --from-repofile=https://repository.mullvad.net/rpm/stable/mullvad.repo --overwrite
+        case debian ubuntu
+            sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc \
+                https://repository.mullvad.net/deb/mullvad-keyring.asc
+            echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc \
+  arch=$( dpkg --print-architecture )] \
+  https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" \
+                | sudo tee /etc/apt/sources.list.d/mullvad.list
+            sudo apt-get update
+        case opensuse-tumbleweed opensuse-leap
+            sudo zypper addrepo -f https://repository.mullvad.net/rpm/stable/mullvad.repo
+        case '*'
+            echo "ERROR: Unable to enable Mullvad repo"
     end
 end
 ##
 ### Installation
 echo ""
+echo "Installing dot files"
+git clone --depth 1 https://github.com/kratss/dotfiles.git >/dev/null
+rm -rf ./dotfiles/.git &>/dev/null
+cp -r ./dotfiles/.* ~/ &>/dev/null
+rm -r ./dotfiles &>/dev/null
+
+echo ""
 echo "Installing packages"
 echo "sudo $mngr install $packages --skip-unavailable"
 sudo $mngr -y install $packages --skip-unavailable
-if test "$_flag_type" = gui
-    echo ""
-    rm -rf ./hawk
-    mkdir ~/.local/bin/ 2>/dev/null
-    git clone https://github.com/kratss/hawk
-    mv ./hawk/hawk.fish ~/.local/bin/
-    mv ./hawk/hawk-preview.fish ~/.local/bin/
-    rm -rf ./haw&k
-end
-
-echo ""
-echo "Installing dot files"
-git clone https://github.com/kratss/dotfiles.git >/dev/null
-echo "Cleaning dot files"
-rm -rf ./dotfiles/.git &>/dev/null
-cp -r ./dotfiles/.* ~/ &>/dev/null
-cd ..
-rm -r ./dotfiles &>/dev/null
-
 if type flatpak &>/dev/null
-    echo ""
-    echo "Installing flatpaks and flatpak repos"
-    flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
-    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-    flatpak install -y fedora com.github.johnfactotum.Foliate
-    flatpak install -y fedora org.gnome.Loupe
-    flatpak install -y fedora org.gnome.Evince
-
-    flatpak install -y fedora org.qutebrowser.qutebrowser
-    flatpak override --user --filesystem=~/.private/kagi org.qutebrowser.qutebrowser # Let flatpak qutebrowser read config
-    flatpak override --user --filesystem=~/.config/qutebrowser org.qutebrowser.qutebrowser
-    rm -rf ~/.var/app/org.qutebrowser.qutebrowser/config/qutebrowser
-    ln -s ~/.config/qutebrowser ~/.var/app/org.qutebrowser.qutebrowser/config/qutebrowser
-
-    flatpak install -y im.riot.Riot
-    flatpak install -y org.gnome.Podcasts
+    for flatpak_package in $flatpaks
+        $flatpak_package
+    end
 end
-##
+if test "$_flag_type" = gui
+    curl -o ~/.local/bin/hawk.fish https://raw.githubusercontent.com/kratss/hawk/refs/heads/master/hawk.fish
+    curl -o ~/.local/bin/hawk-preview.fish https://raw.githubusercontent.com/kratss/hawk/refs/heads/master/hawk-preview.fish
+end
+
 ##
 ### Systemctl
 echo ""
 
+# Helper function to enable the service name passed to it
+function enable_service -a service -a user_flag
+    echo "Enabling $service.service"
+    systemctl $user_flag enable $service.service
+    systemctl $user_flag start $service.service
+    systemctl $user_flag status $service.service | head -n 3
+end
 # Disable GDM login manager
 # Login managers cause issue with sway reading $PATH correcily
 if contains sway $packages
@@ -193,40 +126,25 @@ end
 
 # Syncthing
 if contains syncthing $packages
-    echo "Enabling syncthing.service"
-    systemctl --user enable syncthing.service
-    systemctl --user start syncthing.service
-    systemctl --user status syncthing.service | head -n 3
+    enable_service syncthing --user
 end
 
 # Bluetooth
 if contains bluez $packages
-    echo "Enabling bluetooth.service"
-    systemctl enable bluetooth.service
-    systemctl start bluetooth.service
-    systemctl status bluetooth.service | head -n 3
+    enable_service bluez
 end
 
 # Matrix server updater
 if contains all $_flag_type
-    echo "Enabling matrix-updater.service"
-    systemctl --user enable matrix-updater.serivce
-    systemctl --user status matrix-updater.service | head -n 3
+    enable_service matrix-updater --user
 end
 ##
 ### lazyvim
-# nvim.appimage because apt distros have ancient version of nvim
-if type apt 2>/dev/null; and not test -e ~/.local/bin/nvim.appimage
-    wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-    chmod +x nvim.appimage
-    mkdir ~/.local/bin/
-    mv nvim.appimage ~/.local/bin/
-end
-
 # Get nerdfonts for pretty glyphs
 if type apt 2>/dev/null
     wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/0xProto.zip
     sudo unzip *.zip -d /usr/share/fonts/nerd
+    rm -r 0xProto
 end
 ##
 swaymsg reload
